c154f79b8fc75e3b72c92d3fbdaa2738a4af3353
Stefano Cianciulli <stefano.cianciulli@gmail.com>
2021-10-11 23:02:51 +0100

    Add code for Chapter 11: "Positionable Camera"

--- app/src/main/kotlin/io/github/cianciustyles/App.kt
+++ app/src/main/kotlin/io/github/cianciustyles/App.kt
@@ -19,17 +19,23 @@ fun main() {
 
     val materialGround = Lambertian(Color(0.8, 0.8, 0.0))
     val materialCenter = Lambertian(Color(0.1, 0.2, 0.5))
-    val materialLeft = Dielectric(1.5)
-    val materialRight = Metal(Color(0.8, 0.6, 0.2), 0.0)
+    val materialLeft   = Dielectric(1.5)
+    val materialRight  = Metal(Color(0.8, 0.6, 0.2), 0.0)
 
-    world.add(Sphere(Point3(0.0, -100.5, -1.0), 100.0, materialGround))
-    world.add(Sphere(Point3(0.0, 0.0, -1.0), 0.5, materialCenter))
-    world.add(Sphere(Point3(-1.0, 0.0, -1.0), 0.5, materialLeft))
-    world.add(Sphere(Point3(-1.0, 0.0, -1.0), -0.4, materialLeft))
-    world.add(Sphere(Point3(1.0, 0.0, -1.0), 0.5, materialRight))
+    world.add(Sphere(Point3( 0.0, -100.5, -1.0), 100.0, materialGround))
+    world.add(Sphere(Point3( 0.0,    0.0, -1.0),   0.5, materialCenter))
+    world.add(Sphere(Point3(-1.0,    0.0, -1.0),   0.5, materialLeft))
+    world.add(Sphere(Point3(-1.0,    0.0, -1.0), -0.45, materialLeft))
+    world.add(Sphere(Point3( 1.0,    0.0, -1.0),   0.5, materialRight))
 
     // Camera
-    val camera = Camera()
+    val camera = Camera(
+        Point3(-2.0,2.0,1.0),
+        Point3(0.0,0.0,-1.0),
+        Vector3(0.0,1.0,0.0),
+        20.0,
+        aspectRatio
+    )
 
     // Render
     File("image.ppm").bufferedWriter().use { image ->
